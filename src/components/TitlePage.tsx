ae6b7deed91c2aafbc64d39c89c984b15abda54e
Alex Taylor <alexetaylor7@gmail.com>
2023-12-18 19:10:44 -0700

    added componenets

---
+++ src/components/TitlePage.tsx
@@ -0,0 +1,12 @@
+function TitlePage() {
+  return (
+    <section className="title-page">
+      <div className="title-content">
+        <h1>Alex Taylor</h1>
+        <p>Web Developer | Software Engineer</p>
+      </div>
+    </section>
+  );
+}
+
+export default TitlePage;
