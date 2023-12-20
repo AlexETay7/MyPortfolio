240ee7e3a8dccbd01e68c4a03914e41cec0307dc
Alex Taylor <alexetaylor7@gmail.com>
2023-12-19 23:56:18 -0700

    added accordian dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles/TitlePage.css";
 import TitlePage from "./components/TitlePage";
+import Accordian from "./components/Accordian";
 
 function App() {
   return (
     <div>
       <TitlePage />
+      <Accordian />
     </div>
   );
 }
