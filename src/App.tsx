6c6ce815096cb37fdfdc2c14e9e1555d9e81840a
Alex Taylor <alexetaylor7@gmail.com>
2023-12-20 00:23:44 -0700

    removed accordian

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./styles/TitlePage.css";
 import TitlePage from "./components/TitlePage";
-import Accordian from "./components/Accordian";
 
 function App() {
   return (
     <div>
       <TitlePage />
-      <Accordian />
     </div>
   );
 }
